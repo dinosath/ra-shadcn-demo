a3c6952aab81501fc17faf0a1df547d23c431aff
Konstantinos Athanasiou <dinosath0@gmail.com>
2025-01-30 19:48:23 +0200

    migrate to tailwind 4 and add components from shadcn tailwind 4 starter

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import { ThemeProvider } from "@/components/theme-provider"
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+
+    <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <App />
+        </ThemeProvider>
+
   </React.StrictMode>,
 )
